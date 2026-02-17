a2e70987a3dea3c8e0c28da5141ba47ae8c5149b
ipcamx21 <ipcamx21@gmail.com>
2026-02-16 22:21:48 -0300

    Modify root route to handle 404 responses
    Updated root route to return a custom 404 page.

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const proxyHandler = require('./api/index');
 
 const PORT = process.env.PORT || 3000;
 
-// Rota principal do Proxy
+// Rota principal do Proxy (Continua funcionando)
 app.get('/api', proxyHandler);
 
-// Rota para checar se está online
-app.get('/', (req, res) => {
-    res.send('Proxy Online - Koyeb/Render');
+// Rota Raiz e Qualquer outra página - Disfarce 404 Nginx
+app.get('*', (req, res) => {
+    res.status(404).send(`<html>
+<head><title>404 Not Found</title></head>
+<body bgcolor="white">
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>`);
 });
 
 app.listen(PORT, () => {
